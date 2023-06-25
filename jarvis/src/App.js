c691c4a0c56fa8435e4968c5aae00dbb883e51b3
vikassuresh4397 <vikassuresh4397@gmail.com>
2023-06-25 14:54:03 +0530

    react app.js

--- jarvis/src/App.js
+++ jarvis/src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  // Redirect to the desired URL when the component is mounted
+  window.location.href = 'https://robominds.netlify.app/';
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Content of your component */}
     </div>
   );
 }
